483f3528bd0b5cd8f1e97edf12589b1c77b8df8e
glopesdev <glopesdev@users.noreply.github.com>
2021-04-05 15:50:41 +0100

    Add bash output

--- experiment0.py
+++ experiment0.py
@@ -3,4 +3,4 @@ import aeon
 root = '/ceph/aeon/test2/data'
 data = aeon.sessiondata(root)
 data = data[data.id.str.startswith('BAA')].groupby('id')
-data.apply(lambda g:g[:].drop('id', axis=1))
+print(data.apply(lambda g:g[:].drop('id', axis=1)))
