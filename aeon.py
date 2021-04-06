938312e8f1886d44559ede1ec2c5d3c3157d8629
glopesdev <glopesdev@users.noreply.github.com>
2021-04-07 00:22:04 +0100

    Add support for computing total distance travelled

--- aeon.py
+++ aeon.py
@@ -205,6 +205,25 @@ def harpdata(path, device, register, names=None, start=None, end=None):
         start=start,
         end=end)
 
+def distancetravelled(angle, radius=4.0):
+    '''
+    Calculates the total distance travelled on the wheel, by taking into account
+    its radius and the total number of turns in both directions across time.
+
+    :param Series angle: A series of magnetic encoder measurements.
+    :param float radius: The radius of the wheel, in metric units.
+    :return: The total distance travelled on the wheel, in metric units.
+    '''
+    maxvalue = int(np.iinfo(np.uint16).max >> 2)
+    jumpthreshold = maxvalue // 2
+    turns = angle.astype(int).diff()
+    clickup = (turns < -jumpthreshold).astype(int)
+    clickdown = (turns > jumpthreshold).astype(int) * -1
+    turns = (clickup + clickdown).cumsum()
+    distance = 2 * np.pi * radius * (turns + angle / maxvalue)
+    distance = distance - distance[0]
+    return distance
+
 def sessionduration(data):
     '''
     Computes duration and summary metadata for each session, by subtracting the
