171c37ef53caa84366264ac7f13fb71d8fe3a2aa
Joaquin Rapela <joacorapela@gmail.com>
2021-05-28 20:35:15 +0100

    Replaced argument positions in get_positions_labels by x,y

--- aeon/preprocess/utils.py
+++ aeon/preprocess/utils.py
@@ -1,26 +1,25 @@
 import pdb
 import numpy as np
 
-def get_positions_labels(positions, patches_coordinates=None,
+def get_positions_labels(x, y, patches_coordinates=None,
                          nest_coordinates=None):
 
-    def flag_positions_in_square(positions, square_coordinates):
-        samples = ((square_coordinates["lower_x"]<=positions["x"]) &
-                   (positions["x"]<square_coordinates["higher_x"]) &
-                   (square_coordinates["lower_y"]<=positions["y"]) &
-                   (positions["y"]<square_coordinates["higher_y"]))
-        samples = samples.to_numpy()
+    def flag_positions_in_square(x, y, square_coordinates):
+        samples = ((square_coordinates["lower_x"]<=x) &
+                   (x<square_coordinates["higher_x"]) &
+                   (square_coordinates["lower_y"]<=y) &
+                   (y<square_coordinates["higher_y"]))
         return samples
 
-    labels = np.array([None]*positions.shape[0])
+    labels = np.array([None]*len(x))
     if patches_coordinates is not None:
         for i in range(patches_coordinates.shape[0]):
-            flags = flag_positions_in_square(positions=positions,
+            flags = flag_positions_in_square(x=x, y=y,
                                             square_coordinates=
                                             patches_coordinates.iloc[i,:])
             labels[flags] = "Patch{:d}".format(i)
     if nest_coordinates is not None:
-        flags = flag_positions_in_square(positions=positions,
+        flags = flag_positions_in_square(x=x, y=y,
                                         square_coordinates=
                                         nest_coordinates)
         labels[flags] = "Nest"
