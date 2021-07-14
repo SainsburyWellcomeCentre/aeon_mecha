2807e7ca57ccf0aac3a13d774fd3912673eb4b59
Jai <j.bhagat@ucl.ac.uk>
2021-07-14 16:12:11 +0100

    added 'datajoint' to dependencies

---
+++ docs/examples/jai_160721_sc_meeting_analysis.py
@@ -0,0 +1,8 @@
+import math
+import datetime
+import numpy as np
+import pandas as pd
+from pandas.tseries import frequencies
+import aeon.query.api as api
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
