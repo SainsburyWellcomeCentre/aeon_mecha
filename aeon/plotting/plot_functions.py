952736082c0c51c9a5728ec3f6a8acc71441fa4d
Joaquin Rapela <joacorapela@gmail.com>
2021-08-03 16:07:07 +0100

    Simple change

--- aeon/plotting/plot_functions.py
+++ aeon/plotting/plot_functions.py
@@ -10,9 +10,6 @@ def get_trayectory_trace(x, y, time_stamps=None, sample_rate=None,
         trace = go.Scatter(x=x, y=y, mode="markers")
     else:
         if sample_rate is not None:
-            min_time = time_stamps.min()
-            max_time = time_stamps.max()
-            dt = 1.0/sample_rate
             # we need to remove nan from x and y before calling splprep
             nan_indices = set(np.where(np.isnan(x))[0])
             nan_indices.update(np.where(np.isnan(y))[0])
@@ -22,6 +19,9 @@ def get_trayectory_trace(x, y, time_stamps=None, sample_rate=None,
             time_stamps = np.delete(time_stamps, nan_indices_list)
             # done removing nan from x and y
             tck, u = scipy.interpolate.splprep([x, y], s=0, u=time_stamps)
+            min_time = time_stamps.min()
+            max_time = time_stamps.max()
+            dt = 1.0/sample_rate
             time_stamps = np.arange(min_time, max_time, dt)
             x, y = scipy.interpolate.splev(time_stamps, tck)
         trace = go.Scatter(x=x, y=y, mode="markers",
