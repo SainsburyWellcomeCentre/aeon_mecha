84455c14e7d129dafe038a65c137d94e852526ce
Joseph Burling <josephburling@gmail.com>
2021-08-16 15:55:24 -0500

    update README

--- docker/compose.sh
+++ docker/compose.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# change to location of `docker-compose.yml` before running.
-
 THIS_FILE=$(basename "$0")
 ARG1="$1"
+DEFAULT_KEY=~/.ssh/aeon_mecha
+USAGE="
+NOTE: Change to location of 'docker-compose.yml' before running
+Usage: ./$THIS_FILE \"~/.ssh/my_deploy_key\"
+- If ./$THIS_FILE has no input argument, the ssh key is pulled from $DEFAULT_KEY
+- To docker-compose down: ./$THIS_FILE -d
+
+"
 
 if [[ $ARG1 = "--help" ]] || [[ $ARG1 = "-h" ]]; then
-    echo -e "\nNOTE: Change to location of 'docker-compose.yml' before running\n"
-    echo "usage: ./$THIS_FILE \"~/.ssh/my_deploy_key\""
-    echo "  if ./$THIS_FILE has no input argument, the ssh key is pulled from ~/.ssh/github"
-    echo -e "  to docker-compose down: ./$THIS_FILE -d\n"
+    echo "$USAGE"
     exit
 fi
 
@@ -19,13 +22,20 @@ if [[ $ARG1 = "-d" ]]; then
 fi
 
 if [[ -z $ARG1 ]]; then
-    SSH_KEY="$(cat ~/.ssh/github)"
-    echo "Using private key from ~/.ssh/github"
+    SSH_KEY="$(cat $DEFAULT_KEY)"
+    echo "Using private key from $DEFAULT_KEY"
 else
     SSH_KEY="$(cat $ARG1)"
     echo "Using private key from $ARG1"
 fi
 
+# echo $SSH_KEY
+
+if [[ ! -f "docker-compose.yml" ]]; then
+    echo "$USAGE"
+    echo "ERROR! docker-compose.yml file not found in current location"
+    exit 1
+fi
 
 docker-compose build --build-arg SSH_KEY="${SSH_KEY}"
 docker-compose up -d
