e77b99c39e56c4a00fb406ac89460f03fcac8445
glopesdev <glopesdev@users.noreply.github.com>
2022-03-29 18:55:37 +0000

    Remove unused function

--- aeon/io/utils.py
+++ aeon/io/utils.py
@@ -14,28 +14,3 @@ def getPairedEvents(metadata):
         else:
             i += 1
     return paired_events
-
-def get_positions_labels(x, y, patches_coordinates=None,
-                         nest_coordinates=None):
-
-    def flag_positions_in_square(x, y, square_coordinates):
-        samples = ((square_coordinates["lower_x"].item()<=x) &
-                   (x<square_coordinates["higher_x"].item()) &
-                   (square_coordinates["lower_y"].item()<=y) &
-                   (y<square_coordinates["higher_y"].item()))
-        return samples
-
-    labels = np.array([None]*len(x))
-    if patches_coordinates is not None:
-        for i in range(patches_coordinates.shape[0]):
-            flags = flag_positions_in_square(x=x, y=y,
-                                            square_coordinates=
-                                            patches_coordinates.iloc[i,:])
-            labels[flags] = "Patch{:d}".format(i+1)
-    if nest_coordinates is not None:
-        flags = flag_positions_in_square(x=x, y=y,
-                                        square_coordinates=
-                                        nest_coordinates)
-        labels[flags] = "Nest"
-    labels[np.equal(labels, None)] = "Other"
-    return labels
