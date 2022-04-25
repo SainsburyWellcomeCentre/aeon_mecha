d72e28975657b86679771df4f65df1eac07c247d
Jai <jkbhagatio@gmail.com>
2022-04-25 21:46:10 +0100

    updated config files

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 """
-    Legacy setup file for aeon_mecha.
-    Use setup.cfg to configure this project.
+Minimal file for running `python setup.py develop`.
 
-    This file should be deleted after this PR is released:
-      https://github.com/python-poetry/poetry-core/pull/182
+Legacy setup file for aeon_mecha.
+Use setup.cfg to configure this project.
+
+This file should be deleted after this PR is released:
+https://github.com/python-poetry/poetry-core/pull/182
 """
 from setuptools import setup
 
