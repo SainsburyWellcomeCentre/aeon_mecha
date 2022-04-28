4c24bb0c3f8ff58c0c2b58539881646352c6dbc7
glopesdev <glopesdev@users.noreply.github.com>
2022-04-28 12:56:50 +0000

    Allow arbitrary onset and offset event names

--- aeon/io/utils.py
+++ aeon/io/utils.py
@@ -19,27 +19,40 @@ def distancetravelled(angle, radius=4.0):
     distance = distance - distance[0]
     return distance
 
-def visits(data):
+def visits(data, onset='Enter', offset='Exit'):
     '''
-    Computes duration, enter and exit times for a subject visit. Allows for missing data by
-    trying to match subject enter times with subsequent exit times. If the match fails,
-    subject exit metadata is filled with NaN. Any additional metadata columns in the data
+    Computes duration, onset and offset times from paired events. Allows for missing data
+    by trying to match event onset times with subsequent offset times. If the match fails,
+    event offset metadata is filled with NaN. Any additional metadata columns in the data
     frame will be paired and included in the output.
 
-    :param DataFrame data: A pandas data frame containing subject enter and exit events.
+    :param DataFrame data: A pandas data frame containing visit onset and offset events.
+    :param str, optional onset: The label used to identify event onsets.
+    :param str, optional offset: The label used to identify event offsets.
     :return: A pandas data frame containing duration and metadata for each visit.
     '''
-    enter = data.event == 'Enter'
-    exit = data[enter.shift(1, fill_value=False)].reset_index()
-    enter = data[enter].reset_index()
-    data = enter.join(exit, lsuffix='_enter', rsuffix='_exit')
-    valid_subjects = (data.id_enter == data.id_exit) & (data.event_exit == 'Exit')
+    lonset = onset.lower()
+    loffset = offset.lower()
+    lsuffix = '_{0}'.format(lonset)
+    rsuffix = '_{0}'.format(loffset)
+    id_onset = 'id' + lsuffix
+    id_offset = 'id' + rsuffix
+    event_onset = 'event' + lsuffix
+    event_offset = 'event' + rsuffix
+    time_onset = 'time' + lsuffix
+    time_offset = 'time' + rsuffix
+
+    data_onset = data.event == onset
+    data_offset = data[data_onset.shift(1, fill_value=False)].reset_index()
+    data_onset = data[data_onset].reset_index()
+    data = data_onset.join(data_offset, lsuffix=lsuffix, rsuffix=rsuffix)
+    valid_subjects = (data[id_onset] == data[id_offset]) & (data[event_offset] == offset)
     if ~valid_subjects.any():
         data_types = data.dtypes
-        data.loc[~valid_subjects, [name for name in data.columns if '_exit' in name]] = None
+        data.loc[~valid_subjects, [name for name in data.columns if rsuffix in name]] = None
         data = data.astype(data_types)
-    data['duration'] = data.time_exit - data.time_enter
-    data.rename({ 'time_enter':'enter', 'id_enter':'id', 'time_exit':'exit'}, axis=1, inplace=True)
-    data = data[['id'] + [name for name in data.columns if '_' in name] + ['enter', 'exit', 'duration']]
-    data.drop(['id_exit', 'event_enter', 'event_exit'], axis=1, inplace=True)
+    data['duration'] = data[time_offset] - data[time_onset]
+    data.rename({ time_onset:lonset, id_onset:'id', time_offset:loffset}, axis=1, inplace=True)
+    data = data[['id'] + [name for name in data.columns if '_' in name] + [lonset, loffset, 'duration']]
+    data.drop([id_offset, event_onset, event_offset], axis=1, inplace=True)
     return data
