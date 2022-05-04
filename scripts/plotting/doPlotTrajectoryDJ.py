2f3ec41c269e8ac1ae0452c40f0d21277fe86293
Joaquin Rapela <joacorapela@gmail.com>
2022-05-04 10:51:49 +0100

    Substantial changes

--- scripts/plotting/doPlotTrajectoryDJ.py
+++ scripts/plotting/doPlotTrajectoryDJ.py
@@ -5,8 +5,8 @@ import pandas as pd
 import argparse
 import MySQLdb
 import plotly.graph_objects as go
-
 import datajoint as dj
+
 sys.path.append("../..")
 import aeon.plotting.plot_functions
 
@@ -15,8 +15,14 @@ def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("--mouse_label", help="Mouse label", type=str,
                         default="BAA-1099791")
-    parser.add_argument("--time_slice_start", help="Time slice start",
+    parser.add_argument("--session_start_time", help="session start time",
                         type=str, default="2021-10-01 13:03:45.835619")
+    parser.add_argument("--start_secs",
+                        help="Start plotting start_secs after the start of the session",
+                        type=float, default=0.0)
+    parser.add_argument("--duration_secs",
+                        help="Plot duration_sec seconds",
+                        type=float, default=-1.0)
     parser.add_argument("--sample_rate", help="Plot sample rate", type=float,
                         default=20.0)
     parser.add_argument("--tunneled_host", help="Tunneled host IP address",
@@ -27,28 +33,24 @@ def main(argv):
                         default="rapela")
     parser.add_argument("--db_user_password", help="DB user password",
                         type=str, default="rapela-aeon")
-    parser.add_argument("--db_name", help="DB name", type=str,
-                        default="aeon_tracking")
-    parser.add_argument("--db_table", help="DB table", type=str,
-                        default="_subject_position")
     parser.add_argument("--patches_coordinates", help="coordinates of patches", default="584,597,815,834;614,634,252,271")
     parser.add_argument("--nest_coordinates", help="coordinates of nest", default="170,260,450,540")
     parser.add_argument("--xlabel", help="xlabel", default="x (pixels)")
     parser.add_argument("--ylabel", help="ylabel", default="y (pixels)")
-    parser.add_argument("--title_pattern", help="title pattern", default="Mouse {:s}, time_slice_start {:s}, sample_rate={:.02f} Hz")
-    parser.add_argument("--fig_filename_pattern", help="figure filename pattern", default="../../figures/trajectory_mouse{:s}_timeSliceStart{:s}_srate{:.02f}.{:s}")
+    parser.add_argument("--title_pattern", help="title pattern", default="Mouse {:s}, session_start_time {:s}, start_secs={:.02f}, duration_secs={:02f}, sample_rate={:.02f} Hz")
+    parser.add_argument("--fig_filename_pattern", help="figure filename pattern", default="../../figures/trajectory_mouse{:s}_sessionStart{:s}_startSecs_{:02f}_durationSecs_{:02f}_srate{:.02f}.{:s}")
 
     args = parser.parse_args()
 
     mouse_label = args.mouse_label
-    time_slice_start = args.time_slice_start
+    session_start_time_str = args.session_start_time
+    start_secs = args.start_secs
+    duration_secs = args.duration_secs
     sample_rate = args.sample_rate
     tunneled_host=args.tunneled_host
     db_server_port = args.db_server_port
     db_user = args.db_user
     db_user_password = args.db_user_password
-    db_name = args.db_name
-    db_table = args.db_table
     patches_coordinates_matrix = np.matrix(args.patches_coordinates)
     nest_coordinates_matrix = np.matrix(args.nest_coordinates)
     xlabel = args.xlabel
@@ -67,21 +69,40 @@ def main(argv):
                            port=db_server_port, user=db_user,
                            passwd=db_user_password)
     cur = conn.cursor()
-    sql_stmt = "SELECT timestamps, position_x, position_y FROM {:s}.{:s} WHERE subject=\"{:s}\" AND time_slice_start>=\"{:s}\"".format(db_name, db_table, mouse_label, time_slice_start)
+    sql_stmt = "SELECT timestamps, position_x, position_y FROM aeon_tracking._subject_position WHERE subject=\"{:s}\" AND session_start=\"{:s}\"".format(mouse_label, session_start_time_str)
 
     cur.execute(sql_stmt)
-    row = cur.fetchone()
-    timestamps_blob = row[0]
-    position_x_blob = row[1]
-    position_y_blob = row[2]
-    time_stamps = dj.blob.Blob().unpack(blob=timestamps_blob)
-    x = dj.blob.Blob().unpack(blob=position_x_blob)
-    y = dj.blob.Blob().unpack(blob=position_y_blob)
-
+    records = cur.fetchall()
+    time_stamps = x = y = None
+    time_stamps = np.array([], dtype=object)
+    x = np.array([], dtype=np.double)
+    y = np.array([], dtype=np.double)
+    for row in records:
+        timestamps_blob = row[0]
+        position_x_blob = row[1]
+        position_y_blob = row[2]
+        time_stamps = np.append(time_stamps,
+                                dj.blob.Blob().unpack(blob=timestamps_blob))
+        x = np.append(x, dj.blob.Blob().unpack(blob=position_x_blob))
+        y = np.append(y, dj.blob.Blob().unpack(blob=position_y_blob))
+    cur.close()
+    conn.close()
     time_stamps0_sec = time_stamps[0].timestamp()
-    time_stamps_secs = [ts.timestamp()-time_stamps0_sec for ts in time_stamps]
+    time_stamps_secs = np.array([ts.timestamp()-time_stamps0_sec
+                                 for ts in time_stamps])
+    if duration_secs<0:
+        max_secs = time_stamps_secs.max()
+    else:
+        max_secs = start_secs+duration_secs
+    indices_keep = np.where(
+        np.logical_and(start_secs<=time_stamps_secs,
+                       time_stamps_secs<max_secs))[0]
+    time_stamps_secs = time_stamps_secs[indices_keep]
+    time_stamps = time_stamps[indices_keep]
+    x = x[indices_keep]
+    y = y[indices_keep]
 
-    title = title_pattern.format(mouse_label, time_slice_start, sample_rate)
+    title = title_pattern.format(mouse_label, session_start_time_str, start_secs, duration_secs, sample_rate)
     fig = go.Figure()
     trajectory_trace = aeon.plotting.plot_functions.get_trayectory_trace(x=x, y=y, time_stamps=time_stamps_secs, sample_rate=sample_rate)
     fig.add_trace(trajectory_trace)
@@ -92,8 +113,8 @@ def main(argv):
     fig.add_trace(nest_trace)
     fig.update_layout(title=title, xaxis_title=xlabel, yaxis_title=ylabel, paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)')
 
-    fig.write_image(fig_filename_pattern.format(mouse_label, time_slice_start, sample_rate, "png"))
-    fig.write_html(fig_filename_pattern.format(mouse_label, time_slice_start, sample_rate, "html"))
+    fig.write_image(fig_filename_pattern.format(mouse_label, session_start_time_str, start_secs, duration_secs, sample_rate, "png"))
+    fig.write_html(fig_filename_pattern.format(mouse_label, session_start_time_str, start_secs, duration_secs, sample_rate, "html"))
     pdb.set_trace()
 
 
