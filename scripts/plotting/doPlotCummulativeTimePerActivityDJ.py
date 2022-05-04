941629051da5237e7c7cd63f90120dcc9219220b
Joaquin Rapela <joacorapela@gmail.com>
2022-05-04 10:52:57 +0100

    Substantial changes

--- scripts/plotting/doPlotCummulativeTimePerActivityDJ.py
+++ scripts/plotting/doPlotCummulativeTimePerActivityDJ.py
@@ -19,8 +19,14 @@ def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("--mouse_label", help="Mouse label", type=str,
                         default="BAA-1099791")
-    parser.add_argument("--time_slice_start", help="Time slice start",
+    parser.add_argument("--session_start_time", help="Chunk start",
                         type=str, default="2021-10-01 13:03:45.835619")
+    parser.add_argument("--start_secs",
+                        help="Start plotting start_secs after the start of the session",
+                        type=float, default=0.0)
+    parser.add_argument("--duration_secs",
+                        help="Plot duration_sec seconds",
+                        type=float, default=-1.0)
     parser.add_argument("--tunneled_host", help="Tunneled host IP address",
                         type=str, default="127.0.0.1")
     parser.add_argument("--db_server_port", help="Database server port",
@@ -37,13 +43,15 @@ def main(argv):
     parser.add_argument("--nest_coordinates", help="coordinates of nest", default="170,260,450,540")
     parser.add_argument("--ylabel", help="ylabel", default="Proportion of Time")
     parser.add_argument("--ylim", help="ylim", default="[0,1]")
-    parser.add_argument("--title_pattern", help="title pattern", default="Mouse {:s}, time_slice_start {:s}")
-    parser.add_argument("--fig_filename_pattern", help="figure filenampattern", default="../../figures/activity_times_mouse{:s}_timeSliceStart{:s}.{:s}")
+    parser.add_argument("--title_pattern", help="title pattern", default="Mouse {:s}, session_start_time {:s}, start_secs={:.02f}, duration_secs={:02f}")
+    parser.add_argument("--fig_filename_pattern", help="figure filename pattern", default="../../figures/activityTimes_mouse{:s}_sessionStart{:s}_startSecs_{:02f}_durationSecs_{:02f}.{:s}")
 
     args = parser.parse_args()
 
     mouse_label = args.mouse_label
-    time_slice_start = args.time_slice_start
+    session_start_time = args.session_start_time
+    start_secs = args.start_secs
+    duration_secs = args.duration_secs
     tunneled_host=args.tunneled_host
     db_server_port = args.db_server_port
     db_user = args.db_user
@@ -61,14 +69,38 @@ def main(argv):
                            port=db_server_port, user=db_user,
                            passwd=db_user_password)
     cur = conn.cursor()
-    sql_stmt = "SELECT position_x, position_y FROM {:s}.{:s} WHERE subject=\"{:s}\" AND time_slice_start>=\"{:s}\"".format(db_name, db_table, mouse_label, time_slice_start)
+    sql_stmt = "SELECT timestamps, position_x, position_y FROM {:s}.{:s} WHERE subject=\"{:s}\" AND session_start=\"{:s}\"".format(db_name, db_table, mouse_label, session_start_time)
 
     cur.execute(sql_stmt)
-    row = cur.fetchone()
-    position_x_blob = row[0]
-    position_y_blob = row[1]
-    x = dj.blob.Blob().unpack(blob=position_x_blob)
-    y = dj.blob.Blob().unpack(blob=position_y_blob)
+    records = cur.fetchall()
+    time_stamps = x = y = None
+    time_stamps = np.array([], dtype=object)
+    x = np.array([], dtype=np.double)
+    y = np.array([], dtype=np.double)
+    for row in records:
+        timestamps_blob = row[0]
+        position_x_blob = row[1]
+        position_y_blob = row[2]
+        time_stamps = np.append(time_stamps,
+                                dj.blob.Blob().unpack(blob=timestamps_blob))
+        x = np.append(x, dj.blob.Blob().unpack(blob=position_x_blob))
+        y = np.append(y, dj.blob.Blob().unpack(blob=position_y_blob))
+    cur.close()
+    con.close()
+    time_stamps0_sec = time_stamps[0].timestamp()
+    time_stamps_secs = np.array([ts.timestamp()-time_stamps0_sec
+                                 for ts in time_stamps])
+    if duration_secs<0:
+        max_secs = time_stamps_secs.max()
+    else:
+        max_secs = start_secs + duration_secs
+    indices_keep = np.where(
+        np.logical_and(start_secs<=time_stamps_secs,
+                       time_stamps_secs<max_secs))[0]
+    time_stamps_secs = time_stamps_secs[indices_keep]
+    time_stamps = time_stamps[indices_keep]
+    x = x[indices_keep]
+    y = y[indices_keep]
 
     patches_coordinates = pd.DataFrame(data=patches_coordinates_matrix,
                                        columns=["lower_x", "higher_x",
@@ -80,7 +112,7 @@ def main(argv):
         x=x, y=y,
         patches_coordinates=patches_coordinates,
         nest_coordinates=nest_coordinates)
-    title = title_pattern.format(mouse_label, time_slice_start)
+    title = title_pattern.format(mouse_label, session_start_time, start_secs, duration_secs)
     fig = go.Figure()
     cumTimePerActivity_trace = aeon.plotting.plot_functions.get_cumTimePerActivity_barplot_trace(
         positions_labels=positions_labels,
@@ -88,8 +120,8 @@ def main(argv):
     fig.add_trace(cumTimePerActivity_trace)
     fig.update_layout(title=title, yaxis_title=ylabel)
     fig.update_yaxes(range=ylim)
-    fig.write_image(fig_filename_pattern.format(mouse_label, time_slice_start, "png"))
-    fig.write_html(fig_filename_pattern.format(mouse_label, time_slice_start, "html"))
+    fig.write_image(fig_filename_pattern.format(mouse_label, session_start_time, start_secs, duration_secs, "png"))
+    fig.write_html(fig_filename_pattern.format(mouse_label, session_start_time, start_secs, duration_secs, "html"))
     pdb.set_trace()
 
 if __name__=="__main__":
