79b1d15af588fd1c24848f4cf4bd03f7210377ff
Thinh Nguyen <thinh@vathes.com>
2022-05-24 14:03:27 -0500

    update to use latest version of `analyze_utils.visit`

--- aeon/dj_pipeline/analysis/visit.py
+++ aeon/dj_pipeline/analysis/visit.py
@@ -116,53 +116,11 @@ class OverlapVisit(dj.Computed):
 # ---- HELPERS ----
 
 
-def get_subject_environment_visits(experiment_name, subject, start=None, end=None):
-    """
-    Function to retrieve the enter/exit times and compute the "visits" to the "environment"
-        for a given subject of an experiment
-    Using "analyze" method: `aeon.analyze.utils.visits()`
-
-    :param str experiment_name: name of the experiment
-    :param str subject: name of the subject to retrieve the visits
-    :param datetime start: start time to search for the visits
-    :param datetime end: end time to search for the visits
-    :return: The dataframe of the subject visits
-    """
-    start = start or "1900-01-01"
-    end = end or "2200-01-01"
-
-    enter_exit_query = (
-        acquisition.SubjectEnterExit.Time * acquisition.EventType
-        & {"experiment_name": experiment_name, "subject": subject}
-        & "event_type in ('SubjectEnteredArena', 'SubjectExitedArena')"
-        & f'enter_exit_time BETWEEN "{start}" AND "{end}"'
-    )
-
-    if not enter_exit_query:
-        return pd.DataFrame()
-
-    enter_exit_df = pd.DataFrame(
-        zip(
-            *enter_exit_query.fetch(
-                "subject", "enter_exit_time", "event_type", order_by="enter_exit_time"
-            )
-        )
-    )
-    enter_exit_df.columns = ["id", "time", "event"]
-    enter_exit_df.set_index("time", inplace=True)
-
-    enter_exit_df.replace("SubjectEnteredArena", "Enter", inplace=True)
-    enter_exit_df.replace("SubjectExitedArena", "Exit", inplace=True)
-
-    subject_visits = analyze_utils.visits(enter_exit_df)
-
-    return subject_visits
-
-
 def ingest_environment_visits(experiment_names=["exp0.2-r0"]):
     """
     Function to ingest into `Visit` and `VisitEnd` for specified experiments (default: 'exp0.2-r0')
     This ingestion routine handles only those "complete" visits, not ingesting any "on-going" visits
+    Using "analyze" method: `aeon.analyze.utils.visits()`
 
     :param list experiment_names: list of names of the experiment to ingest into the Visit table
     """
@@ -175,32 +133,57 @@ def ingest_environment_visits(experiment_names=["exp0.2-r0"]):
             .aggr(VisitEnd & place_key, last_visit="MAX(visit_end)")
             .fetch("last_visit")
         )
-        start = min(subjects_last_visits) if len(subjects_last_visits) else None
-        end = datetime.datetime.now() if start else None
+        start = min(subjects_last_visits) if len(subjects_last_visits) else "1900-01-01"
+        end = datetime.datetime.now() if start else "2200-01-01"
 
-        for subject in (acquisition.Experiment.Subject & exp_key).fetch("subject"):
-            subject_visits = get_subject_environment_visits(
-                experiment_name, subject, start=start, end=end
+        enter_exit_query = (
+            acquisition.SubjectEnterExit.Time * acquisition.EventType
+            & {"experiment_name": experiment_name}
+            & "event_type in ('SubjectEnteredArena', 'SubjectExitedArena')"
+            & f'enter_exit_time BETWEEN "{start}" AND "{end}"'
+        )
+
+        if not enter_exit_query:
+            continue
+
+        enter_exit_df = pd.DataFrame(
+            zip(
+                *enter_exit_query.fetch(
+                    "subject",
+                    "enter_exit_time",
+                    "event_type",
+                    order_by="enter_exit_time",
+                )
             )
-            for _, r in subject_visits.iterrows():
-                with Visit.connection.transaction:
-                    Visit.insert1(
-                        {
-                            **exp_key,
-                            **place_key,
-                            "subject": r.id,
-                            "visit_start": r.enter,
-                        },
-                        skip_duplicates=True,
-                    )
-                    VisitEnd.insert1(
-                        {
-                            **exp_key,
-                            **place_key,
-                            "subject": r.id,
-                            "visit_start": r.enter,
-                            "visit_end": r.exit,
-                            "visit_duration": r.duration.total_seconds() / 3600,
-                        },
-                        skip_duplicates=True,
-                    )
+        )
+        enter_exit_df.columns = ["id", "time", "event"]
+        enter_exit_df.set_index("time", inplace=True)
+
+        subject_visits = analyze_utils.visits(
+            enter_exit_df, onset="SubjectEnteredArena", offset="SubjectExitedArena"
+        )
+
+        for _, r in subject_visits.iterrows():
+            if pd.isna(r.subjectexitedarena):
+                continue
+            with Visit.connection.transaction:
+                Visit.insert1(
+                    {
+                        **exp_key,
+                        **place_key,
+                        "subject": r.id,
+                        "visit_start": r.subjectenteredarena,
+                    },
+                    skip_duplicates=True,
+                )
+                VisitEnd.insert1(
+                    {
+                        **exp_key,
+                        **place_key,
+                        "subject": r.id,
+                        "visit_start": r.subjectenteredarena,
+                        "visit_end": r.subjectexitedarena,
+                        "visit_duration": r.duration.total_seconds() / 3600,
+                    },
+                    skip_duplicates=True,
+                )
