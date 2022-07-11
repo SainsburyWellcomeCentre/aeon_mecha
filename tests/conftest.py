b6af3f5ccb96910e73e5ab8c551088ad7ffdffb3
JaerongA <jaerong.ahn@datajoint.com>
2022-07-11 03:28:05 +0000

    ✅ updated test suites

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,25 +18,28 @@ _tear_down = True
 _populate_settings = {"suppress_errors": True}
 
 
-@pytest.fixture(autouse=True, scope="session")        
+@pytest.fixture(autouse=True, scope="session")
 def test_params():
-    
-    return   {
-    "start_ts": "2022-05-24 08:29:42",
-    "end_ts": "2022-05-24 15:59:00",
-    "experiment_name": "exp0.2-r0",
-    "raw_dir": "aeon/data/raw/AEON2/experiment0.2",
-    "qc_dir": "aeon/data/qc/AEON2/experiment0.2",
-    "subject_count": 5,
-    "epoch_count": 1,
-    "chunk_count": 9,
-    "experiment_log_message_count": 1,
-    "subject_enter_exit_count": 1,
-    "subject_weight_time_count": 1
-    }        
-        
+
+    return {
+        "start_ts": "2022-05-24 08:29:42",
+        "end_ts": "2022-05-24 15:59:00",
+        "experiment_name": "exp0.2-r0",
+        "raw_dir": "aeon/data/raw/AEON2/experiment0.2",
+        "qc_dir": "aeon/data/qc/AEON2/experiment0.2",
+        "subject_count": 5,
+        "epoch_count": 1,
+        "chunk_count": 9,
+        "experiment_log_message_count": 0,
+        "subject_enter_exit_count": 0,
+        "subject_weight_time_count": 0,
+        "camera_qc_count": 56,
+        "camera_tracking_count": 7,
+    }
+
 
 def load_pipeline():
+
     from aeon.dj_pipeline import (
         acquisition,
         analysis,
@@ -57,17 +60,18 @@ def load_pipeline():
         "report": report,
     }
 
-def drop_schema():
-    _pipeline = load_pipeline()
-        
-    _pipeline['report'].schema.drop()
-    _pipeline['analysis'].schema.drop()
-    _pipeline['tracking'].schema.drop()
-    _pipeline['qc'].schema.drop()
-    _pipeline['acquisition'].schema.drop()
-    _pipeline['subject'].schema.drop()
-    _pipeline['lab'].schema.drop()
 
+def drop_schema():
+
+    _pipeline = load_pipeline()
+
+    _pipeline["report"].schema.drop()
+    _pipeline["analysis"].schema.drop()
+    _pipeline["tracking"].schema.drop()
+    _pipeline["qc"].schema.drop()
+    _pipeline["acquisition"].schema.drop()
+    _pipeline["subject"].schema.drop()
+    _pipeline["lab"].schema.drop()
 
 
 @pytest.fixture(autouse=True, scope="session")
@@ -84,30 +88,28 @@ def dj_config():
     return
 
 
-
 @pytest.fixture(autouse=True, scope="session")
 def pipeline(dj_config):
-    
+
     _pipeline = load_pipeline()
-    
-    if len(_pipeline['acquisition'].Experiment()): 
+
+    if len(_pipeline["acquisition"].Experiment()):
         drop_schema()
         _pipeline = load_pipeline()
-    
+
     yield _pipeline
 
     if _tear_down:
 
         drop_schema()
-        
 
 
-@pytest.fixture(autouse=True, scope="session")
+@pytest.fixture(scope="session")
 def exp_creation(test_params, pipeline):
     from aeon.dj_pipeline.populate import create_experiment_02
 
     create_experiment_02.main()
-    
+
     acquisition = pipeline["acquisition"]
 
     experiment_name = acquisition.Experiment.fetch1("experiment_name")
@@ -128,34 +130,49 @@ def exp_creation(test_params, pipeline):
             "directory_path": test_params["qc_dir"],
         }
     )
-    
+
     return
 
 
-@pytest.fixture
-def epoch_chunk_ingestion(test_params, pipeline):
+@pytest.fixture(scope="session")
+def epoch_chunk_ingestion(test_params, pipeline, exp_creation):
     acquisition = pipeline["acquisition"]
-    
+
     test_params["experiment_name"]
-    
+
     acquisition.Epoch.ingest_epochs(
         experiment_name=test_params["experiment_name"],
-        start=test_params["start_ts"], end=test_params["end_ts"]
+        start=test_params["start_ts"],
+        end=test_params["end_ts"],
     )
 
-    acquisition.Chunk.ingest_chunks(
-        experiment_name=test_params["experiment_name"]
-    )
+    acquisition.Chunk.ingest_chunks(experiment_name=test_params["experiment_name"])
 
     return
 
 
-@pytest.fixture
-def experimentlog_ingestion(pipeline):
+@pytest.fixture(scope="session")
+def experimentlog_ingestion(pipeline, epoch_chunk_ingestion):
     acquisition = pipeline["acquisition"]
 
     acquisition.ExperimentLog.populate(**_populate_settings)
     acquisition.SubjectEnterExit.populate(**_populate_settings)
     acquisition.SubjectWeight.populate(**_populate_settings)
+
+    return
+
+
+@pytest.fixture(scope="session")
+def camera_qc_ingestion(pipeline, epoch_chunk_ingestion):
+    qc = pipeline["qc"]
+    qc.CameraQC.populate()
+
+    return
+
+
+@pytest.fixture(scope="session")
+def camera_tracking_ingestion(pipeline, camera_qc_ingestion):
+    tracking = pipeline["tracking"]
+    tracking.CameraTracking.populate(display_progress=True)
 
     return
--- tests/dj_pipeline/test_ingestion.py
+++ tests/dj_pipeline/test_ingestion.py
@@ -1,47 +1,65 @@
-from . import (
-    dj_config,
-    pipeline,
-    new_experiment,
-    test_variables,
-    epoch_chunk_ingestion,
-    experimentlog_ingestion,
-)
+from pytest import mark
 
 
-def test_epoch_chunk_ingestion(epoch_chunk_ingestion, test_variables, pipeline):
+@mark.ingestion
+def test_epoch_chunk_ingestion(test_params, pipeline, epoch_chunk_ingestion):
     acquisition = pipeline["acquisition"]
 
     assert (
-        len(acquisition.Epoch & {"experiment_name": test_variables["experiment_name"]})
-        == test_variables["epoch_count"]
+        len(acquisition.Epoch & {"experiment_name": test_params["experiment_name"]})
+        == test_params["epoch_count"]
     )
     assert (
-        len(acquisition.Chunk & {"experiment_name": test_variables["experiment_name"]})
-        == test_variables["chunk_count"]
+        len(acquisition.Chunk & {"experiment_name": test_params["experiment_name"]})
+        == test_params["chunk_count"]
     )
 
 
-def test_experimentlog_ingestion(experimentlog_ingestion, test_variables, pipeline):
+@mark.experimentlog
+def test_experimentlog_ingestion(test_params, pipeline, experimentlog_ingestion):
     acquisition = pipeline["acquisition"]
 
     assert (
         len(
             acquisition.ExperimentLog.Message
-            & {"experiment_name": test_variables["experiment_name"]}
+            & {"experiment_name": test_params["experiment_name"]}
         )
-        == test_variables["experiment_log_message_count"]
+        == test_params["experiment_log_message_count"]
     )
     assert (
         len(
             acquisition.SubjectEnterExit.Time
-            & {"experiment_name": test_variables["experiment_name"]}
+            & {"experiment_name": test_params["experiment_name"]}
         )
-        == test_variables["subject_enter_exit_count"]
+        == test_params["subject_enter_exit_count"]
     )
     assert (
         len(
             acquisition.SubjectWeight.WeightTime
-            & {"experiment_name": test_variables["experiment_name"]}
+            & {"experiment_name": test_params["experiment_name"]}
         )
-        == test_variables["subject_weight_time_count"]
+        == test_params["subject_weight_time_count"]
     )
+
+
+@mark.qc
+def test_camera_qc_ingestion(test_params, pipeline, camera_qc_ingestion):
+
+    qc = pipeline["qc"]
+
+    assert len(qc.CameraQC()) == test_params["camera_qc_count"]
+
+
+@mark.tracking
+def test_camera_tracking_ingestion(
+    test_params,
+    pipeline,
+    camera_tracking_ingestion,
+):
+
+    tracking = pipeline["tracking"]
+
+    import pdb
+
+    pdb.set_trace()
+    assert len(tracking.CameraTracking()) == test_params["camera_tracking_count"]
