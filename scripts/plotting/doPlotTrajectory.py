b46819ebedfae04b7d51fd4f72f6fcf3f81f0f0d
Joaquin Rapela <joacorapela@gmail.com>
2022-07-19 11:53:01 +0100

    Added start and end times to getSessionPositions

--- scripts/plotting/doPlotTrajectory.py
+++ scripts/plotting/doPlotTrajectory.py
@@ -14,8 +14,8 @@ def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("--session_start_time", help="session start time",
                         type=str, default="2021-10-01 13:03:45.835619")
-    parser.add_argument("--start_secs",
-                        help="Start plotting start_secs after the start of the session",
+    parser.add_argument("--start_offset_secs",
+                        help="Start plotting start_offset_secs after the start of the session",
                         type=float, default=0.0)
     parser.add_argument("--duration_secs",
                         help="Plot duration_sec seconds",
@@ -24,7 +24,7 @@ def main(argv):
                         default=20.0)
     parser.add_argument("--storageMgr_type",
                         help="Type of storage manager (SQL | Files)",
-                        type=str, default="Files")
+                        type=str, default="SQL")
     parser.add_argument("--tunneled_host", help="Tunneled host IP address",
                         type=str, default="127.0.0.1")
     parser.add_argument("--db_server_port", help="Database server port",
@@ -39,13 +39,13 @@ def main(argv):
     parser.add_argument("--nest_coordinates", help="coordinates of nest", default="170,260,450,540")
     parser.add_argument("--xlabel", help="xlabel", default="x (pixels)")
     parser.add_argument("--ylabel", help="ylabel", default="y (pixels)")
-    parser.add_argument("--title_pattern", help="title pattern", default="session_start_time={:s}, start_secs={:.02f}, duration_secs={:02f}, sample_rate={:.02f} Hz, storage={:s}")
+    parser.add_argument("--title_pattern", help="title pattern", default="session_start_time={:s}, start_offset_secs={:.02f}, duration_secs={:02f}, sample_rate={:.02f} Hz, storage={:s}")
     parser.add_argument("--fig_filename_pattern", help="figure filename pattern", default="../../figures/trajectory_sessionStart{:s}_startSecs_{:02f}_durationSecs_{:02f}_srate{:.02f}_storage{:s}.{:s}")
 
     args = parser.parse_args()
 
     session_start_time_str = args.session_start_time
-    start_secs = args.start_secs
+    start_offset_secs = args.start_offset_secs
     duration_secs = args.duration_secs
     sample_rate = args.sample_rate
     storageMgr_type = args.storageMgr_type
@@ -78,26 +78,29 @@ def main(argv):
     else:
         raise ValueError(f"Invalid storageMgr_type={storageMgr_type}")
 
-    positions = storageMgr.getSessionPositions(session_start_time_str=session_start_time_str)
+    positions = storageMgr.getSessionPositions(
+        session_start_time_str=session_start_time_str,
+        start_offset_secs=start_offset_secs,
+        duration_secs=duration_secs)
     time_stamps = positions.index
     x = positions["x"].to_numpy()
     y = positions["y"].to_numpy()
     time_stamps0_sec = time_stamps[0].timestamp()
     time_stamps_secs = np.array([ts.timestamp()-time_stamps0_sec
                                  for ts in time_stamps])
-    if duration_secs<0:
-        max_secs = time_stamps_secs.max()
-    else:
-        max_secs = start_secs+duration_secs
-    indices_keep = np.where(
-        np.logical_and(start_secs<=time_stamps_secs,
-                       time_stamps_secs<max_secs))[0]
-    time_stamps_secs = time_stamps_secs[indices_keep]
-    time_stamps = time_stamps[indices_keep]
-    x = x[indices_keep]
-    y = y[indices_keep]
+#     if duration_secs<0:
+#         max_secs = time_stamps_secs.max()
+#     else:
+#         max_secs = start_offset_secs+duration_secs
+#     indices_keep = np.where(
+#         np.logical_and(start_offset_secs<=time_stamps_secs,
+#                        time_stamps_secs<max_secs))[0]
+#     time_stamps_secs = time_stamps_secs[indices_keep]
+#     time_stamps = time_stamps[indices_keep]
+#     x = x[indices_keep]
+#     y = y[indices_keep]
 
-    title = title_pattern.format(session_start_time_str, start_secs, duration_secs, sample_rate, storageMgr_type)
+    title = title_pattern.format(session_start_time_str, start_offset_secs, duration_secs, sample_rate, storageMgr_type)
     storageMgr_type = args.storageMgr_type
     fig = go.Figure()
     trajectory_trace = aeon.plotting.plot_functions.get_trayectory_trace(x=x, y=y, time_stamps=time_stamps_secs, sample_rate=sample_rate)
@@ -110,10 +113,10 @@ def main(argv):
     fig.update_layout(title=title, xaxis_title=xlabel, yaxis_title=ylabel, paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)')
 
     fig.write_image(fig_filename_pattern.format(session_start_time_str,
-                                                start_secs, duration_secs,
+                                                start_offset_secs, duration_secs,
                                                 sample_rate, storageMgr_type, "png"))
     fig.write_html(fig_filename_pattern.format(session_start_time_str,
-                                               start_secs, duration_secs,
+                                               start_offset_secs, duration_secs,
                                                sample_rate, storageMgr_type, "html"))
     import pdb; pdb.set_trace()
 
