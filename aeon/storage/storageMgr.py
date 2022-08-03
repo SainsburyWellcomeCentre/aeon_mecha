8fa90827b1d5989b4937d506d6e9f38534a67d74
Joaquin Rapela <joacorapela@gmail.com>
2022-08-03 09:53:06 +0100

    Adjusted to change in dt API, added getExperimentsNames. getSubjectsNames and getSubjectSessionsStartTimes

--- aeon/storage/storageMgr.py
+++ aeon/storage/storageMgr.py
@@ -7,16 +7,37 @@ class StorageMgr(abc.ABC):
     def getSessionPositions(self, session_start_time_str):
         pass
 
+
     @abc.abstractmethod
     def getSessionEndTime(self, session_start_time_str):
         pass
 
+
     @abc.abstractmethod
     def getWheelAngles(self, start_time_str, end_time_str, patch_label):
         pass
+
 
     @abc.abstractmethod
     def getFoodPatchEventTimes(self, start_time_str, end_time_str, event_label, patch_label):
         pass
 
 
+    @abc.abstractmethod
+    def getExperimentsNames(self, directory_type="raw"):
+        pass
+
+
+    @abc.abstractmethod
+    def getSubjectsNames(self, experiment_name):
+        pass
+
+
+    @abc.abstractmethod
+    def getSessionDuration(self, experiment_name, subject, session_start):
+        pass
+
+
+    @abc.abstractmethod
+    def getSubjectSessionsStartTimes(self, experiment_name, subject):
+        pass
