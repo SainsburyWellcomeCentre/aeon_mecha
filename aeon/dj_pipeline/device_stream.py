aba5ba2c1dd855eb091f8db005c3757263d502bb
Thinh Nguyen <thinh@vathes.com>
2022-11-17 17:59:04 -0600

    auto ingest device types and stream types

--- aeon/dj_pipeline/device_stream.py
+++ aeon/dj_pipeline/device_stream.py
@@ -6,9 +6,11 @@ import re
 
 import aeon
 from aeon.io import api as io_api
+import aeon.schema.core as stream
+import aeon.schema.foraging as foraging
+import aeon.schema.octagon as octagon
 
-from . import acquisition
-from . import get_schema_name
+from . import acquisition, dict_to_uuid, get_schema_name
 
 logger = dj.logger
 
@@ -29,100 +31,61 @@ class StreamType(dj.Lookup):
     """
 
     definition = """  # Catalog of all stream types used across Project Aeon
-    stream_type: varchar(16)
+    stream_type: varchar(20)
     ---
     stream_reader: varchar(256)     # name of the reader class found in `aeon_mecha` package (e.g. aeon.io.reader.Video)
     stream_reader_kwargs: longblob  # keyword arguments to instantiate the reader class
     stream_description='': varchar(256)
+    stream_hash: uuid  # hash of dict(stream_reader_kwargs, stream_reader=stream_reader)
+    unique index (stream_hash)
     """
 
-    contents = [
-        ("Video", "aeon.io.reader.Video", {"pattern": "{}"}, "Video frame metadata"),
-        (
-            "Position",
-            "aeon.io.reader.Position",
-            {"pattern": "{}_200"},
-            "Position tracking data for the specified camera.",
-        ),
-        (
-            "Encoder",
-            "aeon.io.reader.Encoder",
-            {"pattern": "{}_90"},
-            "Wheel magnetic encoder data",
-        ),
-        (
-            "EnvironmentState",
-            "aeon.io.reader.Csv",
-            {"pattern": "{}_EnvironmentState", "columns": ["state"]},
-            "Environment state log",
-        ),
-        (
-            "SubjectState",
-            "aeon.io.reader.Subject",
-            {"pattern": "{}_SubjectState"},
-            "Subject state log",
-        ),
-        (
-            "MessageLog",
-            "aeon.io.reader.Log",
-            {"pattern": "{}_MessageLog"},
-            "Message log data",
-        ),
-        (
-            "Metadata",
-            "aeon.io.reader.Metadata",
-            {"pattern": "{}"},
-            "Metadata for acquisition epochs",
-        ),
-        (
-            "MetadataExp01",
-            "aeon.io.reader.Metadata",
-            {"pattern": "{}_2", "columns": ["id", "weight", "event"]},
-            "Session metadata for Experiment 0.1",
-        ),
-        (
-            "Region",
-            "aeon.schema.foraging._RegionReader",
-            {"pattern": "{}_201"},
-            "Region tracking data for the specified camera",
-        ),
-        (
-            "DepletionState",
-            "aeon.schema.foraging._PatchState",
-            {"pattern": "{}_State"},
-            "State of the linear depletion function for foraging patches",
-        ),
-        (
-            "BeamBreak",
-            "aeon.io.reader.BitmaskEvent",
-            {"pattern": "{}_32", "value": 0x22, "tag": "PelletDetected"},
-            "Beam break events for pellet detection",
-        ),
-        (
-            "DeliverPellet",
-            "aeon.io.reader.BitmaskEvent",
-            {"pattern": "{}_35", "value": 0x80, "tag": "TriggerPellet"},
-            "Pellet delivery commands",
-        ),
-        (
-            "WeightRaw",
-            "aeon.schema.foraging._Weight",
-            {"pattern": "{}_200"},
-            "Raw weight measurement for a specific nest",
-        ),
-        (
-            "WeightFiltered",
-            "aeon.schema.foraging._Weight",
-            {"pattern": "{}_202"},
-            "Filtered weight measurement for a specific nest",
-        ),
-        (
-            "WeightSubject",
-            "aeon.schema.foraging._Weight",
-            {"pattern": "{}_204"},
-            "Subject weight measurement for a specific nest",
-        ),
-    ]
+    @classmethod
+    def insert_streams(cls, *streams):
+        composite = {}
+        pattern = "{pattern}"
+        for stream_obj in streams:
+            if inspect.isclass(stream_obj):
+                for method in vars(stream_obj).values():
+                    if isinstance(method, staticmethod):
+                        composite.update(method.__func__(pattern))
+            else:
+                composite.update(stream_obj(pattern))
+
+        stream_entries = []
+        for stream_name, stream_reader in composite.items():
+            if stream_name == pattern:
+                stream_name = stream_reader.__class__.__name__
+            entry = {
+                "stream_type": stream_name,
+                "stream_reader": f"{stream_reader.__module__}.{stream_reader.__class__.__name__}",
+                "stream_reader_kwargs": {
+                    k: v
+                    for k, v in vars(stream_reader).items()
+                    if k
+                    in inspect.signature(stream_reader.__class__.__init__).parameters
+                },
+            }
+            entry["stream_hash"] = dict_to_uuid(
+                {
+                    **entry["stream_reader_kwargs"],
+                    "stream_reader": entry["stream_reader"],
+                }
+            )
+            q_param = cls & {"stream_hash": entry["stream_hash"]}
+            if q_param:  # If the specified stream type already exists
+                pname = q_param.fetch1("stream_type")
+                if pname != stream_name:
+                    # If the existed stream type does not have the same name:
+                    #   human error, trying to add the same content with different name
+                    raise dj.DataJointError(
+                        f"The specified stream type already exists - name: {pname}"
+                    )
+
+            stream_entries.append(entry)
+
+        cls.insert(stream_entries, skip_duplicates=True)
+        return stream_entries
 
 
 @schema
@@ -143,37 +106,70 @@ class DeviceType(dj.Lookup):
         -> StreamType
         """
 
+    _devices_config = [
+        (
+            "Camera",
+            "Camera device",
+            (stream.video, stream.position, foraging.region),
+        ),
+        ("Metadata", "Metadata", (stream.metadata,)),
+        (
+            "ExperimentalMetadata",
+            "ExperimentalMetadata",
+            (stream.environment, stream.messageLog),
+        ),
+        (
+            "Nest Scale",
+            "Weight scale at nest",
+            (foraging.weight,),
+        ),
+        (
+            "Food Patch",
+            "Food patch",
+            (foraging.patch,),
+        ),
+        (
+            "Food Patch",
+            "Food patch",
+            (foraging.patch,),
+        ),
+        (
+            "Photodiode",
+            "Photodiode",
+            (octagon.photodiode,),
+        ),
+        (
+            "OSC",
+            "OSC",
+            (octagon.OSC,),
+        ),
+        (
+            "TaskLogic",
+            "TaskLogic",
+            (octagon.TaskLogic,),
+        ),
+        (
+            "Wall",
+            "Wall",
+            (octagon.Wall,),
+        ),
+    ]
+
     @classmethod
-    def _insert_contents(cls):
-        devices_config = [
-            ("Camera", "Camera device", ("Video", "Position", "Region")),
-            ("Metadata", "Metadata", ("Metadata",)),
-            (
-                "ExperimentalMetadata",
-                "ExperimentalMetadata",
-                ("EnvironmentState", "SubjectState", "MessageLog"),
-            ),
-            (
-                "Nest Scale",
-                "Weight scale at nest",
-                ("WeightRaw", "WeightFiltered", "WeightSubject"),
-            ),
-            (
-                "Food Patch",
-                "Food patch",
-                ("DepletionState", "Encoder", "BeamBreak", "DeliverPellet"),
-            ),
-        ]
-        for device_type, device_desc, device_streams in devices_config:
-            if cls & {"device_type": device_type}:
-                continue
+    def insert_devices(cls):
+        for device_type, device_desc, device_streams in cls._devices_config:
+            stream_entries = StreamType.insert_streams(*device_streams)
             with cls.connection.transaction:
-                cls.insert1((device_type, device_desc))
+                cls.insert1((device_type, device_desc), skip_duplicates=True)
                 cls.Stream.insert(
                     [
-                        {"device_type": device_type, "stream_type": stream_type}
-                        for stream_type in device_streams
-                    ]
+                        {
+                            "device_type": device_type,
+                            "stream_type": e["stream_type"],
+                        }
+                        for e in stream_entries
+                    ],
+                    skip_duplicates=True,
                 )
 
 
@@ -184,9 +180,6 @@ class Device(dj.Lookup):
     ---
     -> DeviceType
     """
-
-
-DeviceType._insert_contents()
 
 
 # ---- HELPER ----
@@ -253,7 +246,7 @@ def generate_device_table(device_type, context=None):
             -> Experiment{device_title}
             -> acquisition.Chunk
             ---
-            size: int              # number of data points acquired from this stream for a given chunk
+            sample_count: int      # number of data points acquired from this stream for a given chunk
             timestamps: longblob   # (datetime) timestamps of {stream_type} data
             """
 
@@ -294,7 +287,7 @@ def generate_device_table(device_type, context=None):
 
                 stream = self._stream_reader(
                     **{
-                        k: v.format(device_name) if k == "pattern" else v
+                        k: v.format(**{k: device_name}) if k == "pattern" else v
                         for k, v in self._stream_detail["stream_reader_kwargs"].items()
                     }
                 )
@@ -331,8 +324,16 @@ def _prettify(s):
     return s.replace("_", " ").title().replace(" ", "")
 
 
+# ---- MAIN BLOCK ----
+
+
 def main():
+    DeviceType.insert_devices()
+
     context = inspect.currentframe().f_back.f_locals
     for device_type in DeviceType.fetch("device_type"):
         logger.info(f"Generating stream table(s) for: {device_type}")
         generate_device_table(device_type, context=context)
+
+
+main()
