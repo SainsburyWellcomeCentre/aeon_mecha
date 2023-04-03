2d4bef9116912f7e80ceed3606b3baf44d62c52f
glopesdev <glopesdev@users.noreply.github.com>
2023-04-03 10:35:58 +0000

    Add regression tests for partial time range filter

--- tests/io/test_api.py
+++ tests/io/test_api.py
@@ -4,6 +4,20 @@ from aeon.schema.dataset import exp02
 import pandas as pd
 
 class TestLoad(unittest.TestCase):
+    def test_load_start_only(self):
+        data = aeon.load(
+            './tests/data/nonmonotonic',
+            exp02.Patch2.Encoder,
+            start=pd.Timestamp('2022-06-06T13:00:49'))
+        self.assertGreater(len(data), 0, "data range is empty")
+
+    def test_load_end_only(self):
+        data = aeon.load(
+            './tests/data/nonmonotonic',
+            exp02.Patch2.Encoder,
+            end=pd.Timestamp('2022-06-06T13:00:49'))
+        self.assertGreater(len(data), 0, "data range is empty")
+
     def test_load_monotonic(self):
         data = aeon.load('./tests/data/monotonic', exp02.Patch2.Encoder)
         self.assertTrue(data.index.is_monotonic_increasing)
