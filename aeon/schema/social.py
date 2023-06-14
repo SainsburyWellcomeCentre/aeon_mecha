13b7bc8a80cdf9d48b35988ab281f0646a50b937
Jai <jkbhagatio@gmail.com>
2023-06-14 23:13:03 +0000

    Drafted Sleap reader and class (subject) int2str converter

--- aeon/schema/social.py
+++ aeon/schema/social.py
@@ -9,9 +9,19 @@ class Sleap(_reader.Harp):
     def __init__(self, model_path):
         """Initializes a reader for Harp-binarized Sleap tracking data given a Sleap network model path."""
         columns = ["class", "class_likelihood"]
-        # @todo Get bodyparts from Sleap config.
-        bodyparts = []
+        # Get bodyparts from Sleap config.
+        with open(model_path) as f:
+            config = json.load(f)
+        bodyparts = config["model"]["heads"]["multi_class_topdown"]["confmaps"]["bodyparts"]
         for part in bodyparts:
             columns.extend([f"{part}_x", f"{part}_y", f"{part}_likelihood"])
         super().__init__(pattern="", columns=columns)
-        
+
+
+def sleap_class_int2str(tracking_df, model_path):
+    """Converts a SLEAP class integer in a tracking data dataframe to its associated string (subject id)."""
+    with open(model_path) as f:
+        config = json.load(f)
+    class_names = config["model"]["heads"]["multi_class_topdown"]["class_vectors"]["classes"]
+    for i, subj in enumerate(class_names):
+        tracking_df.loc[tracking_df["class"] == i, "class"] = subj
