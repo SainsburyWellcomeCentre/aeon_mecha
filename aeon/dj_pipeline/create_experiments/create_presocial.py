95c8dfcbab0a531bd2e92786d794cb902a479fff
JaerongA <jaerong.ahn@datajoint.com>
2023-06-28 15:12:47 +0000

    allow ingestion from multiple machines

--- aeon/dj_pipeline/create_experiments/create_presocial.py
+++ aeon/dj_pipeline/create_experiments/create_presocial.py
@@ -1,9 +1,9 @@
 from aeon.dj_pipeline import acquisition, lab, subject
 
-experiment_type = "presocial"
-experiment_name = "presocial0.1-a2"  # AEON2 acquisition computer
+experiment_type = "presocial0.1"
+experiment_names = ["presocial0.1-a2", "presocial0.1-a3", "presocial0.1-a4"]
 location = "4th floor"
-
+computers = ["AEON2", "AEON3", "AEON4"]
 
 def create_new_experiment():
 
@@ -13,22 +13,23 @@ def create_new_experiment():
         {"experiment_type": experiment_type}, skip_duplicates=True
     )
 
-    acquisition.Experiment.insert1(
-        {
+    acquisition.Experiment.insert(
+        [{
             "experiment_name": experiment_name,
             "experiment_start_time": "2023-02-25 00:00:00",
-            "experiment_description": "presocial experiment 0.1 in aeon2",
+            "experiment_description": "presocial experiment 0.1",
             "arena_name": "circle-2m",
             "lab": "SWC",
             "location": location,
-            "experiment_type": experiment_type,
-        },
+            "experiment_type": experiment_type
+        } for experiment_name in experiment_names],
         skip_duplicates=True,
     )
 
     acquisition.Experiment.Subject.insert(
         [
             {"experiment_name": experiment_name, "subject": s}
+            for experiment_name in experiment_names
             for s in subject.Subject.fetch("subject")
         ],
         skip_duplicates=True,
@@ -40,14 +41,8 @@ def create_new_experiment():
                 "experiment_name": experiment_name,
                 "repository_name": "ceph_aeon",
                 "directory_type": "raw",
-                "directory_path": "aeon/data/raw/AEON2/presocial0.1",
-            },
-            {
-                "experiment_name": experiment_name,
-                "repository_name": "ceph_aeon",
-                "directory_type": "quality-control",
-                "directory_path": "aeon/data/qc/AEON2/presocial0.1",
-            },
+                "directory_path": f"aeon/data/raw/{computer}/{experiment_type}"
+            } for experiment_name, computer in zip(experiment_names, computers)
         ],
         skip_duplicates=True,
     )
