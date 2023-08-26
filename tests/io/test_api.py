44efe2de0c988231eac24fc2128901a080c1606f
Jai <jkbhagatio@gmail.com>
2023-08-26 19:31:41 +0000

    Made paths in tests absolute, and allowed for tests to now be imported.

--- tests/io/test_api.py
+++ tests/io/test_api.py
@@ -1,42 +1,47 @@
 import pytest
 from pytest import mark
-import aeon.io.api as aeon
+import aeon.io.api as aeonapi
 from aeon.schema.dataset import exp02
 import pandas as pd
+from pathlib import Path
+
+nonmonotonic_path = Path(__file__).parent.parent / "data" / "nonmonotonic"
+monotonic_path = Path(__file__).parent.parent / "data" / "monotonic"
+
 
 @mark.api
 def test_load_start_only():
-    data = aeon.load(
-        './tests/data/nonmonotonic',
-        exp02.Patch2.Encoder,
-        start=pd.Timestamp('2022-06-06T13:00:49'))
+    data = aeonapi.load(nonmonotonic_path, exp02.Patch2.Encoder, start=pd.Timestamp("2022-06-06T13:00:49"))
     assert len(data) > 0
+
 
 @mark.api
 def test_load_end_only():
-    data = aeon.load(
-        './tests/data/nonmonotonic',
-        exp02.Patch2.Encoder,
-        end=pd.Timestamp('2022-06-06T13:00:49'))
+    data = aeonapi.load(
+        nonmonotonic_path, exp02.Patch2.Encoder, end=pd.Timestamp("2022-06-06T13:00:49")
+    )
     assert len(data) > 0
+
 
 @mark.api
 def test_load_filter_nonchunked():
-    data = aeon.load(
-        './tests/data/nonmonotonic',
-        exp02.Metadata,
-        start=pd.Timestamp('2022-06-06T09:00:00'))
+    data = aeonapi.load(
+        nonmonotonic_path, exp02.Metadata, start=pd.Timestamp("2022-06-06T09:00:00")
+    )
     assert len(data) > 0
+
 
 @mark.api
 def test_load_monotonic():
-    data = aeon.load('./tests/data/monotonic', exp02.Patch2.Encoder)
-    assert data.index.is_monotonic_increasing
+    data = aeonapi.load(monotonic_path, exp02.Patch2.Encoder)
+    assert len(data) > 0 and data.index.is_monotonic_increasing
+
 
 @mark.api
 def test_load_nonmonotonic():
-    data = aeon.load('./tests/data/nonmonotonic', exp02.Patch2.Encoder)
+    data = aeonapi.load(nonmonotonic_path, exp02.Patch2.Encoder)
     assert not data.index.is_monotonic_increasing
-        
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     pytest.main()
