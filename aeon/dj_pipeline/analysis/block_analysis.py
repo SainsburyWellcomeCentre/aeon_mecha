49dc4355a757d4d9c88ad5b2346fc240f86dc198
Thinh Nguyen <thinh@vathes.com>
2024-01-23 17:54:46 -0600

    feat(block_analysis): add analysis and plots for Block

--- aeon/dj_pipeline/analysis/block_analysis.py
+++ aeon/dj_pipeline/analysis/block_analysis.py
@@ -1,6 +1,7 @@
 import datetime
 import datajoint as dj
 import pandas as pd
+import json
 
 from aeon.analysis import utils as analysis_utils
 
@@ -39,6 +40,7 @@ class BlockAnalysis(dj.Computed):
         pellet_timestamps: longblob
         total_distance_travelled: float
         cumulative_distance_travelled: longblob
+        wheel_timestamps: longblob
         """
 
     class Subject(dj.Part):
@@ -70,6 +72,8 @@ class BlockAnalysis(dj.Computed):
             f' AND chunk_start < "{max(end_query.fetch("chunk_end"))}"'
         )
 
+        self.insert1(key)
+
         # Patch data - TriggerPellet, DepletionState, Encoder (distancetravelled)
         maintenance_period = get_maintenance_periods(key["experiment_name"], block_start, block_end)
 
@@ -81,7 +85,6 @@ class BlockAnalysis(dj.Computed):
         )
         patch_keys, patch_names = patch_query.fetch("KEY", "underground_feeder_name")
 
-        food_patch_entries = []
         for patch_key, patch_name in zip(patch_keys, patch_names):
             delivered_pellet_df = fetch_stream(
                 streams.UndergroundFeederBeamBreak & patch_key & time_restriction
@@ -99,15 +102,16 @@ class BlockAnalysis(dj.Computed):
             ]
             # filter out maintenance period based on logs
             encoder_df = filter_out_maintenance_periods(encoder_df, maintenance_period, block_end)
-            distance_travelled = analysis_utils.distancetravelled(encoder_df.angle)
-            food_patch_entries.append(
+            encoder_df["distance_travelled"] = analysis_utils.distancetravelled(encoder_df.angle)
+            self.Patch.insert1(
                 {
                     **key,
                     "patch_name": patch_name,
                     "pellet_count": len(pellet_df),
                     "pellet_timestamps": pellet_df.index.values,
-                    "cumulative_distance_travelled": distance_travelled.values,
-                    "total_distance_travelled": distance_travelled.values[-1],
+                    "cumulative_distance_travelled": encoder_df.distance_travelled.values,
+                    "total_distance_travelled": encoder_df.distance_travelled.values[-1],
+                    "wheel_timestamps": encoder_df.index.values,
                 }
             )
 
@@ -116,7 +120,6 @@ class BlockAnalysis(dj.Computed):
         subject_events_df = fetch_stream(subject_events_query)
 
         subject_names = set(subject_events_df.id)
-        subject_entries = []
         for subject_name in subject_names:
             # positions - query for CameraTop, identity_name matches subject_name,
             pos_query = (
@@ -137,18 +140,80 @@ class BlockAnalysis(dj.Computed):
             weight_df = fetch_stream(weight_query)[block_start:block_end]
             weight_df.query(f"subject_id == '{subject_name}'", inplace=True)
 
-            subject_entries.append(
+            self.Subject.insert1(
                 {
                     **key,
                     "subject_name": subject_name,
                     "weights": weight_df.weight.values,
                     "weight_timestamps": weight_df.index.values,
                     "position_x": pos_df.x.values,
-                    "position_y": pos_df.y.values[-1],
+                    "position_y": pos_df.y.values,
                     "position_likelihood": pos_df.likelihood.values,
                     "position_timestamps": pos_df.index.values,
                 }
             )
+
+
+@schema
+class BlockPlots(dj.Computed):
+    definition = """
+    -> BlockAnalysis
+    ---
+    subject_positions_plot: longblob
+    subject_weights_plot: longblob
+    patch_distance_travelled_plot: longblob
+    """
+
+    def make(self, key):
+        import plotly.graph_objs as go
+
+        # For position data , set confidence threshold to return position values and downsample by 5x
+        conf_thresh = 0.9
+        downsampling_factor = 5
+
+        # Make plotly plots
+        weight_fig = go.Figure()
+        pos_fig = go.Figure()
+        for subject_data in (BlockAnalysis.Subject & key).fetch(as_dict=True):
+            weight_fig.add_trace(
+                go.Scatter(
+                    x=subject_data["weight_timestamps"],
+                    y=subject_data["weights"],
+                    mode="lines",
+                    name=subject_data["subject_name"],
+                )
+            )
+            mask = subject_data["position_likelihood"] > conf_thresh
+            pos_fig.add_trace(
+                go.Scatter3d(
+                    x=subject_data["position_x"][mask][::downsampling_factor],
+                    y=subject_data["position_y"][mask][::downsampling_factor],
+                    z=subject_data["position_timestamps"][mask][::downsampling_factor],
+                    mode="lines",
+                    name=subject_data["subject_name"],
+                )
+            )
+
+        wheel_fig = go.Figure()
+        for patch_data in (BlockAnalysis.Patch & key).fetch(as_dict=True):
+            wheel_fig.add_trace(
+                go.Scatter(
+                    x=patch_data["wheel_timestamps"][::2],
+                    y=patch_data["cumulative_distance_travelled"][::2],
+                    mode="lines",
+                    name=patch_data["patch_name"],
+                )
+            )
+
+        # insert figures as json-formatted plotly plots
+        self.insert1(
+            {
+                **key,
+                "subject_positions_plot": json.loads(pos_fig.to_json()),
+                "subject_weights_plot": json.loads(weight_fig.to_json()),
+                "patch_distance_travelled_plot": json.loads(wheel_fig.to_json()),
+            }
+        )
 
 
 @schema
