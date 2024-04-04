28e53972bfe5d0d40c68627df280d8525a4b6373
Thinh Nguyen <thinh@datajoint.com>
2024-04-03 19:19:05 -0500

    update(schemas): update social schemas to use the refactored Stream, StreamGroup and Device

--- aeon/schema/schemas.py
+++ aeon/schema/schemas.py
@@ -1,48 +1,51 @@
 from dotmap import DotMap
-from aeon.io.device import Device
-from aeon.schema import core, foraging, octagon, social
+
+import aeon.schema.core as stream
+from aeon.schema.streams import Device
+from aeon.schema import foraging, octagon, social_01, social_02
+
 
 exp02 = DotMap(
     [
-        Device("Metadata", core.metadata),
-        Device("ExperimentalMetadata", core.environment, core.message_log),
-        Device("CameraTop", core.video, core.position, foraging.region),
-        Device("CameraEast", core.video),
-        Device("CameraNest", core.video),
-        Device("CameraNorth", core.video),
-        Device("CameraPatch1", core.video),
-        Device("CameraPatch2", core.video),
-        Device("CameraSouth", core.video),
-        Device("CameraWest", core.video),
-        Device("Nest", foraging.weight),
-        Device("Patch1", foraging.patch),
-        Device("Patch2", foraging.patch),
+        Device("Metadata", stream.Metadata),
+        Device("ExperimentalMetadata", stream.Environment, stream.MessageLog),
+        Device("CameraTop", stream.Video, stream.Position, foraging.Region),
+        Device("CameraEast", stream.Video),
+        Device("CameraNest", stream.Video),
+        Device("CameraNorth", stream.Video),
+        Device("CameraPatch1", stream.Video),
+        Device("CameraPatch2", stream.Video),
+        Device("CameraSouth", stream.Video),
+        Device("CameraWest", stream.Video),
+        Device("Nest", foraging.Weight),
+        Device("Patch1", foraging.Patch),
+        Device("Patch2", foraging.Patch),
     ]
 )
 
 exp01 = DotMap(
     [
-        Device("SessionData", foraging.session),
-        Device("FrameTop", core.video, core.position),
-        Device("FrameEast", core.video),
-        Device("FrameGate", core.video),
-        Device("FrameNorth", core.video),
-        Device("FramePatch1", core.video),
-        Device("FramePatch2", core.video),
-        Device("FrameSouth", core.video),
-        Device("FrameWest", core.video),
-        Device("Patch1", foraging.depletion_function, core.encoder, foraging.feeder),
-        Device("Patch2", foraging.depletion_function, core.encoder, foraging.feeder),
+        Device("SessionData", foraging.SessionData),
+        Device("FrameTop", stream.Video, stream.Position),
+        Device("FrameEast", stream.Video),
+        Device("FrameGate", stream.Video),
+        Device("FrameNorth", stream.Video),
+        Device("FramePatch1", stream.Video),
+        Device("FramePatch2", stream.Video),
+        Device("FrameSouth", stream.Video),
+        Device("FrameWest", stream.Video),
+        Device("Patch1", foraging.DepletionFunction, stream.Encoder, foraging.Feeder),
+        Device("Patch2", foraging.DepletionFunction, stream.Encoder, foraging.Feeder),
     ]
 )
 
 octagon01 = DotMap(
     [
-        Device("Metadata", core.metadata),
-        Device("CameraTop", core.video, core.position),
-        Device("CameraColorTop", core.video),
-        Device("ExperimentalMetadata", core.subject_state),
-        Device("Photodiode", octagon.photodiode),
+        Device("Metadata", stream.Metadata),
+        Device("CameraTop", stream.Video, stream.Position),
+        Device("CameraColorTop", stream.Video),
+        Device("ExperimentalMetadata", stream.SubjectState),
+        Device("Photodiode", octagon.Photodiode),
         Device("OSC", octagon.OSC),
         Device("TaskLogic", octagon.TaskLogic),
         Device("Wall1", octagon.Wall),
@@ -58,55 +61,56 @@ octagon01 = DotMap(
 
 social01 = DotMap(
     [
-        Device("Metadata", core.metadata),
-        Device("Environment", social.environment_b, social.subject_b),
-        Device("CameraTop", core.video, social.camera_top_pos_b),
-        Device("CameraNorth", core.video),
-        Device("CameraSouth", core.video),
-        Device("CameraEast", core.video),
-        Device("CameraWest", core.video),
-        Device("CameraPatch1", core.video),
-        Device("CameraPatch2", core.video),
-        Device("CameraPatch3", core.video),
-        Device("CameraNest", core.video),
-        Device("Nest", social.weight_raw_b, social.weight_filtered_b),
-        Device("Patch1", social.patch_streams_b),
-        Device("Patch2", social.patch_streams_b),
-        Device("Patch3", social.patch_streams_b),
-        Device("RfidGate", social.rfid_events_social01_b),
-        Device("RfidNest1", social.rfid_events_social01_b),
-        Device("RfidNest2", social.rfid_events_social01_b),
-        Device("RfidPatch1", social.rfid_events_social01_b),
-        Device("RfidPatch2", social.rfid_events_social01_b),
-        Device("RfidPatch3", social.rfid_events_social01_b),
+        Device("Metadata", stream.Metadata),
+        Device("Environment", social_02.Environment, social_02.SubjectData),
+        Device("CameraTop", stream.Video, social_02.Pose),
+        Device("CameraNorth", stream.Video),
+        Device("CameraSouth", stream.Video),
+        Device("CameraEast", stream.Video),
+        Device("CameraWest", stream.Video),
+        Device("CameraPatch1", stream.Video),
+        Device("CameraPatch2", stream.Video),
+        Device("CameraPatch3", stream.Video),
+        Device("CameraNest", stream.Video),
+        Device("Nest", social_02.WeightRaw, social_02.WeightFiltered),
+        Device("Patch1", social_02.Patch),
+        Device("Patch2", social_02.Patch),
+        Device("Patch3", social_02.Patch),
+        Device("RfidGate", social_01.RfidEvents),
+        Device("RfidNest1", social_01.RfidEvents),
+        Device("RfidNest2", social_01.RfidEvents),
+        Device("RfidPatch1", social_01.RfidEvents),
+        Device("RfidPatch2", social_01.RfidEvents),
+        Device("RfidPatch3", social_01.RfidEvents),
     ]
 )
 
 
 social02 = DotMap(
     [
-        Device("Metadata", core.metadata),
-        Device("Environment", social.environment_b, social.subject_b),
-        Device("CameraTop", core.video, social.camera_top_pos_b),
-        Device("CameraNorth", core.video),
-        Device("CameraSouth", core.video),
-        Device("CameraEast", core.video),
-        Device("CameraWest", core.video),
-        Device("CameraPatch1", core.video),
-        Device("CameraPatch2", core.video),
-        Device("CameraPatch3", core.video),
-        Device("CameraNest", core.video),
-        Device("Nest", social.weight_raw_b, social.weight_filtered_b),
-        Device("Patch1", social.patch_streams_b),
-        Device("Patch2", social.patch_streams_b),
-        Device("Patch3", social.patch_streams_b),
-        Device("Patch1Rfid", social.rfid_events_b),
-        Device("Patch2Rfid", social.rfid_events_b),
-        Device("Patch3Rfid", social.rfid_events_b),
-        Device("NestRfid1", social.rfid_events_b),
-        Device("NestRfid2", social.rfid_events_b),
-        Device("GateRfid", social.rfid_events_b),
+        Device("Metadata", stream.Metadata),
+        Device("Environment", social_02.Environment, social_02.SubjectData),
+        Device("CameraTop", stream.Video, social_02.Pose),
+        Device("CameraNorth", stream.Video),
+        Device("CameraSouth", stream.Video),
+        Device("CameraEast", stream.Video),
+        Device("CameraWest", stream.Video),
+        Device("CameraPatch1", stream.Video),
+        Device("CameraPatch2", stream.Video),
+        Device("CameraPatch3", stream.Video),
+        Device("CameraNest", stream.Video),
+        Device("Nest", social_02.WeightRaw, social_02.WeightFiltered),
+        Device("Patch1", social_02.Patch),
+        Device("Patch2", social_02.Patch),
+        Device("Patch3", social_02.Patch),
+        Device("RfidGate", social_02.RfidEvents),
+        Device("RfidNest1", social_02.RfidEvents),
+        Device("RfidNest2", social_02.RfidEvents),
+        Device("RfidPatch1", social_02.RfidEvents),
+        Device("RfidPatch2", social_02.RfidEvents),
+        Device("RfidPatch3", social_02.RfidEvents),
     ]
 )
+
 
 __all__ = ["exp01", "exp02", "octagon01", "social01", "social02"]
