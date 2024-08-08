833f9e9403a8404cecb65fe02efd895918f0e17f
Thinh Nguyen <thinh@datajoint.com>
2024-08-08 08:52:08 -0500

    feat: add one-off logic to ingest fullpose data for social02

--- aeon/schema/social_02.py
+++ aeon/schema/social_02.py
@@ -44,6 +44,12 @@ class Pose(Stream):
         super().__init__(_reader.Pose(f"{path}_test-node1*"))
 
 
+class Pose03(Stream):
+
+    def __init__(self, path):
+        super().__init__(_reader.Pose(f"{path}_202_*"))
+
+
 class WeightRaw(Stream):
 
     def __init__(self, path):
