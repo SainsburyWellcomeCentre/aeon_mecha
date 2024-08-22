352a41dc99a16f5724dc9a548b1c8a9e385e54b1
Thinh Nguyen <thinh@datajoint.com>
2024-08-22 17:26:43 -0500

    feat(acquisition): add `EnvironmentActiveConfig`

--- aeon/schema/social_03.py
+++ aeon/schema/social_03.py
@@ -1,3 +1,5 @@
+import json
+import pandas as pd
 import aeon.io.reader as _reader
 from aeon.schema.streams import Stream
 
@@ -6,3 +8,23 @@ class Pose(Stream):
 
     def __init__(self, path):
         super().__init__(_reader.Pose(f"{path}_202_*"))
+
+
+class EnvActiveConfigReader(_reader.Reader):
+    def __init__(self, pattern, columns=["name", "value"], extension="jsonl"):
+        super().__init__(pattern, columns, extension)
+
+    def read(self, file):
+        """Reads data from the specified jsonl file."""
+        with open(file, "r") as f:
+            df = pd.read_json(f, lines=True)
+        df["name"] = df["value"].apply(lambda x: x["name"])
+        df["time"] = pd.to_datetime(df["seconds"], unit="s")
+        df.set_index("time", inplace=True)
+        return df[self.columns]
+
+
+class ActiveConfiguration(Stream):
+
+    def __init__(self, path):
+        super().__init__(EnvActiveConfigReader(f"{path}_ActiveConfiguration_*"))
