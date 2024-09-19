23c440f24f92d235a24641e0726f94f9f950debe
glopesdev <glopesdev@users.noreply.github.com>
2024-09-19 16:04:17 +0100

    Ensure all roots are Path objects

--- aeon/io/api.py
+++ aeon/io/api.py
@@ -87,7 +87,7 @@ def load(root, reader, start=None, end=None, time=None, tolerance=None, epoch=No
     fileset = {
         chunk_key(fname): fname
         for path in root
-        for fname in path.glob(f"{epoch_pattern}/**/{reader.pattern}.{reader.extension}")
+        for fname in Path(path).glob(f"{epoch_pattern}/**/{reader.pattern}.{reader.extension}")
     }
     files = sorted(fileset.items())
 
