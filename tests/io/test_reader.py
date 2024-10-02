010fdb9c8b18a5e25fa3f7a2d746a5282619b70f
glopesdev <glopesdev@users.noreply.github.com>
2024-10-02 09:18:25 +0100

    Add regression test for poses with register prefix

--- tests/io/test_reader.py
+++ tests/io/test_reader.py
@@ -4,7 +4,7 @@ import pytest
 from pytest import mark
 
 import aeon
-from aeon.schema.schemas import social02
+from aeon.schema.schemas import social02, social03
 
 pose_path = Path(__file__).parent.parent / "data" / "pose"
 
@@ -12,6 +12,12 @@ pose_path = Path(__file__).parent.parent / "data" / "pose"
 @mark.api
 def test_Pose_read_local_model_dir():
     data = aeon.load(pose_path, social02.CameraTop.Pose)
+    assert len(data) > 0
+
+
+@mark.api
+def test_Pose_read_local_model_dir_with_register_prefix():
+    data = aeon.load(pose_path, social03.CameraTop.Pose)
     assert len(data) > 0
 
 
