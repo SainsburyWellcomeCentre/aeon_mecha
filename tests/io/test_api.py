d6cf52ffc1c305a2202c8cf28ab0b47d59f1c2f9
Jai <jkbhagatio@gmail.com>
2024-10-10 22:52:43 +0100

    updated tests

--- tests/io/test_api.py
+++ tests/io/test_api.py
@@ -6,6 +6,7 @@ from pytest import mark
 
 import aeon
 from aeon.schema.schemas import exp02
+from aeon.schema.ingestion_schemas import social03
 
 nonmonotonic_path = Path(__file__).parent.parent / "data" / "nonmonotonic"
 monotonic_path = Path(__file__).parent.parent / "data" / "monotonic"
@@ -14,7 +15,7 @@ monotonic_path = Path(__file__).parent.parent / "data" / "monotonic"
 @mark.api
 def test_load_start_only():
     data = aeon.load(
-        nonmonotonic_path, exp02.Patch2.Encoder, start=pd.Timestamp("2022-06-06T13:00:49"), downsample=None
+        nonmonotonic_path, exp02.Patch2.Encoder, start=pd.Timestamp("2022-06-06T13:00:49")
     )
     assert len(data) > 0
 
@@ -22,7 +23,7 @@ def test_load_start_only():
 @mark.api
 def test_load_end_only():
     data = aeon.load(
-        nonmonotonic_path, exp02.Patch2.Encoder, end=pd.Timestamp("2022-06-06T13:00:49"), downsample=None
+        nonmonotonic_path, exp02.Patch2.Encoder, end=pd.Timestamp("2022-06-06T13:00:49")
     )
     assert len(data) > 0
 
@@ -35,22 +36,22 @@ def test_load_filter_nonchunked():
 
 @mark.api
 def test_load_monotonic():
-    data = aeon.load(monotonic_path, exp02.Patch2.Encoder, downsample=None)
+    data = aeon.load(monotonic_path, exp02.Patch2.Encoder)
     assert len(data) > 0
     assert data.index.is_monotonic_increasing
 
 
 @mark.api
 def test_load_nonmonotonic():
-    data = aeon.load(nonmonotonic_path, exp02.Patch2.Encoder, downsample=None)
+    data = aeon.load(nonmonotonic_path, exp02.Patch2.Encoder)
     assert not data.index.is_monotonic_increasing
 
 
 @mark.api
 def test_load_encoder_with_downsampling():
     DOWNSAMPLE_PERIOD = 0.02
-    data = aeon.load(monotonic_path, exp02.Patch2.Encoder, downsample=True)
-    raw_data = aeon.load(monotonic_path, exp02.Patch2.Encoder, downsample=None)
+    data = aeon.load(monotonic_path, social03.Patch2.Encoder)
+    raw_data = aeon.load(monotonic_path, exp02.Patch2.Encoder)
 
     # Check that the length of the downsampled data is less than the raw data
     assert len(data) < len(raw_data)
