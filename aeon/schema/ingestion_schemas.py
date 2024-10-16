d5bd0fe735abcd712310c0c19f4be4113f67678b
Thinh Nguyen <thinh@datajoint.com>
2024-10-15 20:22:34 -0500

    feat(ingestion): use new `ingestion_schemas`

--- aeon/schema/ingestion_schemas.py
+++ aeon/schema/ingestion_schemas.py
@@ -1,5 +1,4 @@
 """Aeon experiment schemas for DataJoint database ingestion."""
-
 from os import PathLike
 
 import pandas as pd
@@ -9,7 +8,6 @@ import aeon.schema.core as stream
 from aeon.io import reader
 from aeon.io.api import aeon as aeon_time
 from aeon.schema import foraging, octagon, social_01, social_02, social_03
-from aeon.schema.foraging import DepletionFunction, Feeder
 from aeon.schema.streams import Device, Stream, StreamGroup
 
 
@@ -27,16 +25,27 @@ class _Encoder(reader.Encoder):
         return data
 
 
-class _Video(reader.Video):
-    """A version of the video reader that drops columns that can be recreated from data and metadata."""
+class _Video(reader.Csv):
+    """A version of the video reader that retains only the `hw_timestamp` column."""
 
-    def read(self, file: PathLike[str], drop_cols=None) -> pd.DataFrame:
+    def __init__(self, pattern):
+        super().__init__(pattern, columns=["hw_timestamp"])
+        self._rawcolumns = ["time"] + ['hw_counter', 'hw_timestamp']
+
+    def read(self, file):
         """Reads video metadata from the specified file."""
-        drop_cols = ["hw_counter", "_frame", "_path", "_epoch"] if drop_cols is None else drop_cols
         data = pd.read_csv(file, header=0, names=self._rawcolumns)
+        drop_cols = [c for c in data.columns if c not in self.columns]
         data.drop(columns=drop_cols, errors="ignore", inplace=True)
         data.set_index("time", inplace=True)
         return data
+
+
+class Encoder(Stream):
+    """Wheel magnetic encoder data."""
+
+    def __init__(self, pattern):
+        super().__init__(_Encoder(f"{pattern}_90_*"))
 
 
 # Define new streams and stream groups
@@ -47,18 +56,14 @@ class Video(Stream):
         super().__init__(_Video(f"{pattern}_*"))
 
 
-class Encoder(Stream):
-    """Wheel magnetic encoder data."""
-
-    def __init__(self, pattern):
-        super().__init__(_Encoder(f"{pattern}_90_*"))
-
-
 class Patch(StreamGroup):
     """Data streams for a patch."""
 
-    def __init__(self, pattern):
-        super().__init__(pattern, DepletionFunction, Encoder, Feeder)
+    def __init__(self, path):
+        super().__init__(path)
+
+    p = social_02.Patch
+    e = Encoder
 
 
 # Define schemas
@@ -93,8 +98,8 @@ exp01 = DotMap(
         Device("FramePatch2", Video),
         Device("FrameSouth", Video),
         Device("FrameWest", Video),
-        Device("Patch1", foraging.DepletionFunction, stream.Encoder, foraging.Feeder),
-        Device("Patch2", foraging.DepletionFunction, stream.Encoder, foraging.Feeder),
+        Device("Patch1", foraging.DepletionFunction, Encoder, foraging.Feeder),
+        Device("Patch2", foraging.DepletionFunction, Encoder, foraging.Feeder),
     ]
 )
 
@@ -201,4 +206,35 @@ social03 = DotMap(
 )
 
 
-__all__ = ["octagon01", "exp01", "exp02", "social01", "social02", "social03"]
+social04 = DotMap(
+    [
+        Device("Metadata", stream.Metadata),
+        Device("Environment", social_02.Environment, social_02.SubjectData, social_03.EnvironmentActiveConfiguration),
+        Device("CameraTop", Video, social_03.Pose),
+        Device("CameraNorth", Video),
+        Device("CameraSouth", Video),
+        Device("CameraEast", Video),
+        Device("CameraWest", Video),
+        Device("CameraNest", Video),
+        Device("CameraPatch1", Video),
+        Device("CameraPatch2", Video),
+        Device("CameraPatch3", Video),
+        Device("Nest", social_02.WeightRaw, social_02.WeightFiltered),
+        Device("Patch1", Patch),
+        Device("Patch2", Patch),
+        Device("Patch3", Patch),
+        Device("PatchDummy1", Patch),
+        Device("NestRfid1", social_02.RfidEvents),
+        Device("NestRfid2", social_02.RfidEvents),
+        Device("GateRfid", social_02.RfidEvents),
+        Device("GateEastRfid", social_02.RfidEvents),
+        Device("GateWestRfid", social_02.RfidEvents),
+        Device("Patch1Rfid", social_02.RfidEvents),
+        Device("Patch2Rfid", social_02.RfidEvents),
+        Device("Patch3Rfid", social_02.RfidEvents),
+        Device("PatchDummy1Rfid", social_02.RfidEvents),
+    ]
+)
+
+# __all__ = ["octagon01", "exp01", "exp02", "social01", "social02", "social03", "social04"]
+__all__ = ["social02", "social03", "social04"]
