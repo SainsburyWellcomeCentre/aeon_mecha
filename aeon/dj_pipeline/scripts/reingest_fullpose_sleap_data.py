3e59db8dd65959f32d1f62b54283bf6d6a8d13d2
Thinh Nguyen <thinh@datajoint.com>
2024-10-21 16:08:32 -0500

    Update reingest_fullpose_sleap_data.py

--- aeon/dj_pipeline/scripts/reingest_fullpose_sleap_data.py
+++ aeon/dj_pipeline/scripts/reingest_fullpose_sleap_data.py
@@ -1,15 +1,33 @@
 from datetime import datetime
 from aeon.dj_pipeline import acquisition, tracking
 
+aeon_schemas = acquisition.aeon_schemas
+
 
 exp_key = {"experiment_name": "social0.2-aeon4"}
 
 
 def find_chunks_to_reingest(exp_key, delete_not_fullpose=False):
+    device_name = "CameraTop"
+
+    devices_schema = getattr(
+        aeon_schemas,
+        (acquisition.Experiment.DevicesSchema & {"experiment_name": exp_key["experiment_name"]}).fetch1(
+            "devices_schema_name"
+        ),
+    )
+    stream_reader = getattr(getattr(devices_schema, device_name), "Pose")
+
+    # special ingestion case for social0.2 full-pose data (using Pose reader from social03)
+    if exp_key["experiment_name"].startswith("social0.2"):
+        from aeon.io import reader as io_reader
+        stream_reader = getattr(getattr(devices_schema, device_name), "Pose03")
+        assert isinstance(stream_reader, io_reader.Pose), "Pose03 is not a Pose reader"
+
     # find processed path for exp_key
     processed_dir = acquisition.Experiment.get_data_directory(exp_key, "processed")
 
-    files = sorted(f.stem for f in processed_dir.rglob("CameraTop_202_*.bin") if f.is_file())
+    files = sorted(f.stem for f in processed_dir.rglob(f"{stream_reader.pattern}.bin") if f.is_file())
     # extract timestamps from the file names & convert to datetime
     file_times = [datetime.strptime(f.split("_")[-1], "%Y-%m-%dT%H-%M-%S") for f in files]
 
