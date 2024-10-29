6ad3b0af76fd070cc228701eb5ab1a26ae222c27
MilagrosMarin <milmarina02@gmail.com>
2024-10-29 18:11:14 +0000

    fix: review

--- aeon/dj_pipeline/__init__.py
+++ aeon/dj_pipeline/__init__.py
@@ -6,6 +6,8 @@ import uuid
 
 import datajoint as dj
 
+logger = dj.logger
+
 _default_database_prefix = os.getenv("DJ_DB_PREFIX") or "aeon_"
 _default_repository_config = {"ceph_aeon": "/ceph/aeon"}
 
@@ -68,5 +70,5 @@ except ImportError:
         from .utils import streams_maker
 
         streams = dj.VirtualModule("streams", streams_maker.schema_name)
-    except Exception:
-        pass
+    except Exception as e:
+        logger.debug(f"Could not import streams module: {e}")
