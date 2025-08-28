3b7b29153e720d43623027c70c3939336ae4f3eb
Thinh Nguyen <thinh@datajoint.com>
2025-08-28 10:36:09 -0500

    chore: robust handling of missing `get_repository_path` configuration

--- aeon/schema/social_03.py
+++ aeon/schema/social_03.py
@@ -8,8 +8,11 @@ from aeon.dj_pipeline.utils import paths
 class Pose(Stream):
     def __init__(self, path):
         """Initializes the Pose stream."""
-        model_root = paths.get_repository_path('ceph_aeon') / "aeon" / "data" / "ingest"
-        super().__init__(reader.Pose(f"{path}_222*", model_root=model_root.as_posix()))
+        try:
+            model_root = (paths.get_repository_path('ceph_aeon') / "aeon" / "data" / "ingest").as_posix()
+        except Exception:
+            model_root = "ceph/aeon/aeon/data/ingest"
+        super().__init__(reader.Pose(f"{path}_222*", model_root=model_root))
 
 
 class EnvironmentActiveConfiguration(Stream):
